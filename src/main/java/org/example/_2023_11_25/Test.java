5269f8a2ec1b46b33a821fd0359b8bfd7ed2c350
shonmikl <mikhailegorov676@gmail.com>
2023-11-25 11:48:20 +0100

    ****

--- src/main/java/org/example/_2023_11_25/Test.java
+++ src/main/java/org/example/_2023_11_25/Test.java
@@ -1,13 +1,13 @@
 package org.example._2023_11_25;
 
 public class Test {
-    static int a(int a) {
-        return a * 10;
-    }
-
-    static int b(int b) {
-        return b * 100;
-    }
+//    static int a(int a) {
+//        return a * 10;
+//    }
+//
+//    static int b(int b) {
+//        return b * 100;
+//    }
 
     public static void main(String[] args) {
         m(Test::r);
