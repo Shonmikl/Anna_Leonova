107b37cdf890ff0630e5b5593c22727806a06546
shonmikl <mikhailegorov676@gmail.com>
2023-11-25 12:41:50 +0100

    taski

---
+++ src/main/java/org/example/_2023_11_25/StrEx2.java
@@ -0,0 +1,41 @@
+package org.example._2023_11_25;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StrEx2 {
+    public static void main(String[] args) {
+        /**
+         * mama myla ramu
+         *
+         * из строки - в массив строк по словам !!!!!
+         * запустить стрим на массив строк!!!!!
+         * посчитать гласные
+         * будем хранить в мапе инт инт через группировку
+         * потом просто пройтись фор ичем по мапе
+         *
+         */
+        String text = "mama myla ramu";
+        String[] arr = text.split(" ");
+//        Map<Integer, Integer> map = Arrays.stream(arr)
+//                .collect(Collectors.groupingBy(el -> el, st -> ? ? ??));
+
+
+    }
+
+//    boolean isVowel(char c) {
+//        switch (c) {
+//            case 'a' -> {
+//                return true;
+//            }
+//            case
+//        }
+
+    /**
+     * public List<int[][]> getPoints(List<int[][]>) {
+     *     return null;
+     * }
+     */
+}
+
